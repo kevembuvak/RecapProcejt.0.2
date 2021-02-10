e4b56b9315443cf0de5b1102a88ce680bfd1fe86
Kerem Burak Yılmaz <k.burak02@outlook.com>
2021-02-10 19:59:36 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<Type>
+    {
+        Type GetById(int id);
+        List<Type> GetAll(Expression<Func<Type, bool>> filter = null);
+        Type Get(Expression<Func<Type, bool>> filter);
+        void Add(Type entity);
+        void Delete(Type entity);
+        void Update(Type entity);
+    }
+}
